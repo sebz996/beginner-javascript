e703f4d666c4cafcb90093cc8cb26f978a2ec248
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-06-02 21:50:46 +0800

    module 1-7 complete

--- playground/types.js
+++ playground/types.js
@@ -2,4 +2,6 @@ const name = 'seb';
 const middle = 'tryst';
 const last = `xander`;
 
-const sentence = `she's so "cool"`
+const sentence = `she's so "cool"`;
+
+const hello = `hello my name is ${name}. Nice to meet you.`
