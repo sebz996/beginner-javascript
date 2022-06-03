16312aa074976cb18bec36c17d99c51309bd9b32
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-06-03 20:32:16 +0800

    comlete module 2-16 - debugging

---
+++ exercises/16 - Debugging/debugging.js
@@ -0,0 +1,67 @@
+const people = [
+  { name: 'Wes', cool: true, country: 'Canada' },
+  { name: 'Scott', cool: true, country: 'Merica' },
+  { name: 'Snickers', cool: false, country: 'Dog Country' },
+];
+
+people.forEach((person, index) => {
+  console.log(person.name);
+});
+
+const food = [
+  { name: '肉夹馍', spicy: false, province: '陕西' },
+  { name: '热干面', spicy: false, province: '湖北' },
+  { name: '夫妻肺片', spicy: true, province: '四川' },
+];
+
+console.table(food);
+
+
+// Console Methods
+
+// Callstack
+
+// Grabbing Elements
+
+// Breakpoints
+
+// Scope
+
+// Network Requests
+
+// Break On Attribute
+
+// Some Setup Code
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function greet(name) {
+  doesntExist();
+  return `Hello ${name}`;
+}
+
+function go() {
+  const name = doctorize(greet('Wes'));
+  console.log(name);
+}
+
+const button = document.querySelector('.bigger');
+button.addEventListener('click', function(e) {
+  const newFontSize =
+    parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
+  e.currentTarget.style.fontSize = `${newFontSize}px`;
+});
+
+// A Dad joke fetch
+async function fetchDadJoke() {
+  const res = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'text/plain',
+    },
+  });
+  const joke = await res.text();
+  console.log(joke);
+  return joke;
+}
