87ee5fc58cdc6e6d5bfa306c0eea0832a7e7db42
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-06-03 10:36:13 +0800

    complete module 2-12 - built in functions

--- playground/types.js
+++ playground/types.js
@@ -1,6 +1,6 @@
-let isDrawing = false;
-let age = 18;
-const ofAge = age > 10;
-console.log(ofAge);
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const year = day * 365;
 
-age = 100;
+let years = Math.round(Date.now() / year);
