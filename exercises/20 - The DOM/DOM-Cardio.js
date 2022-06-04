05794dfa165c68cb76b692f3865179520e4bd859
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-06-04 19:40:04 +0800

    complete module 4-28

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,44 +1,58 @@
 // Make a div
+const div = document.createElement('div');
 
 // add a class of wrapper to it
+div.classList.add('wrapper');
 
 // put it into the body
+document.body.appendChild(div);
 
 // make an unordered list
+const ul = `<ul>
+    <li>one</li>
+    <li>two</li>
+    <li>three</li>
+</ul>`;
+
 
 // add three list items with the words "one, two, three" in them
 // put that list into the above wrapper
+div.innerHTML = ul;
+console.log(div);
+
 
 // create an image
+const img = document.createElement('img');
 
 // set the source to an image
+img.src = 'https://place-puppy.com/500x500'
+
 // set the width to 250
+img.width = 250;
+
 // add a class of cute
+img.classList.add('cute');
+
 // add an alt of Cute Puppy
+img.alt = 'cute puppy';
+
 // Append that image to the wrapper
+div.appendChild(img);
 
 // with HTML string, make a div, with two paragraphs inside of it
+const myHTML = `
+<div class="myDiv">
+<p>Paragraph one</p>
+<p>Paragraph two</p>
+</div>
+`;
+
+const ulElement = div.querySelector('ul');
+console.log(ulElement);
+
 // put this div before the unordered list from above
+ulElement.insertAdjacentElement('beforebegin', myHTML);
+
 
 // add a class to the second paragraph called warning
-// remove the first paragraph
-
-// create a function called generatePlayerCard that takes in three arguments: name, age, and height
-
-// have that function return html that looks like this:
-// <div class="playerCard">
-//   <h2>NAME — AGE</h2>
-//   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
-// </div>
-
-// make a new div with a class of cards
-
-// make 4 player cards using generatePlayerCard
-
-// append those cards to the div
-// put the div into the DOM just before the wrapper element
-// Bonus, put a delete Button on each card so when you click it, the whole card is removed
-
-// select all the buttons!
-// make out delete function
-// loop over them and attach a listener
+const myDiv = div.querySelector('myDiv');
