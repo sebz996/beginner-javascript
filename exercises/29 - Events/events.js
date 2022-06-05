c886aa9b40067bb03b9b89a5be57f9623b9cc8b1
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-06-05 13:22:25 +0800

    complete module 5-30 - targets, bubbling, propagation, capture

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -1,23 +1,24 @@
-const butts = document.querySelector('.butts');
-const coolButton = document.querySelector('.cool');
+// const butts = document.querySelector('.butts');
+// const coolButton = document.querySelector('.cool');
 
-function handleClick() {
-    console.log('it got clicked');
-}
+// function handleClick() {
+//     console.log('it got clicked');
+// }
 
-// get sth, listen for sth, do sth
-butts.addEventListener('click', handleClick);
-coolButton.addEventListener('click', handleClick);
+// // get sth, listen for sth, do sth
+// butts.addEventListener('click', handleClick);
+// coolButton.addEventListener('click', handleClick);
 
-butts.removeEventListener('click', handleClick);
+// butts.removeEventListener('click', handleClick);
 
 // listen for events on multiple items
 const buyButtons = document.querySelectorAll('button.buy');
 
-function buyItem() {
-    console.log('buying item');
+function handleBuyButtonClick(event) {
+    console.log("You're buying it!");
+    console.log(event.target.dataset.price);
 }
 
-buyButtons.forEach(function(buyButton) {
-    console.log('Binding the buy botton');
+buyButtons.forEach(function(buyButton){
+    buyButton.addEventListener('click', handleBuyButtonClick);
 })
