6c61c6ecbacf6615c1b923df511acd5b9adacba0
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-06-05 16:59:06 +0800

    complete module 5-32 - accessibility

--- exercises/29 - Events/forms.js
+++ exercises/29 - Events/forms.js
@@ -1,21 +1,34 @@
-const arch = document.querySelector('.archive');
+// const arch = document.querySelector('.archive');
 
-arch.addEventListener('click', function(event) {
+// arch.addEventListener('click', function(event) {
    
-    const shouldChangePage = confirm('This website is not for posers. Do you proceed?');
-    if(!shouldChangePage) {
-        event.preventDefault();
-    }
-    console.log(shouldChangePage);
+//     const shouldChangePage = confirm('This website is not for posers. Do you proceed?');
+//     if(!shouldChangePage) {
+//         event.preventDefault();
+//     }
+//     console.log(shouldChangePage);
     
-});
+// });
 
-const signupForm = document.queryCommandIndeterm('[name="signup"]');
+// const signupForm = document.queryCommandIndeterm('[name="signup"]');
 
-signupForm.addEventListener('submit', function(event) {
-    console.log(event);
-    event.preventDefault();
-    console.log(event.currentTarget.name.value);
-    console.log(event.currentTarget.email.value);
-    console.log(event.currentTarget.agree.checked);
-});
+// signupForm.addEventListener('submit', function(event) {
+//     console.log(event);
+//     event.preventDefault();
+//     console.log(event.currentTarget.name.value);
+//     console.log(event.currentTarget.email.value);
+//     console.log(event.currentTarget.agree.checked);
+// });
+
+const photo = document.querySelector('.photo');
+
+function handlePhotoClick(event) {
+    if(event.type === 'click' || event.key === 'Enter') {
+        
+    }
+    console.log('You clicked the photo');
+    console.log(event.keycode);
+}
+
+photo.addEventListener('click', handlePhotoClick);
+photo.addEventListener('keyup', handlePhotoClick);
