bde32fca811272e98b99fa714734e9edc2a9aa9d
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-06-06 21:56:55 +0800

    module 6-36 tabs - create event listener

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,3 +1,9 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
 const tabPanels = document.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+    console.log(event.currentTarget);
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
