fa23982d74b7073a8262443c5577467fc26b9d90
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-06-06 22:02:44 +0800

    module 6-36 tabs - hide tab panels

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -3,7 +3,14 @@ const tabButtons = tabs.querySelectorAll('[role="tab"]');
 const tabPanels = document.querySelectorAll('[role="tabpanel"]');
 
 function handleTabClick(event) {
-    console.log(event.currentTarget);
+    // hide tab panels
+    tabPanels.forEach(panel => {
+        panel.hidden = true;
+    })
+    // mark all tabs unselected
+    // mark clicked tab as selected
+    // find and show associated tab panel
+
 }
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
